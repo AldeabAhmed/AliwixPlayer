7f493c7b209b15be28085c19d39ea81bdd181beb
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2021-12-01 17:03:58 +0100

    Update jwplayer.js

--- assets/jwplayer.js
+++ assets/jwplayer.js
@@ -5,18 +5,27 @@ c=l.exec(window.location.href);
 return null==c?"https://heny007.github.io/HeNy-Ben-Hamed/today-matches/aliwix.mp4":unescape(c[1])}
 var video=gup("src");
 var iptv=(video);
-jwplayer("app").setup({file:iptv,
+var playerInstance = jwplayer("app").setup({file:iptv,
 title: 'HeNy007',autostart: "false",width: "100%",height: "100%",
 tag:"HeNy007",title:"HeNy007",autostart:"true",stretching:"exactfit",autostretch:"true",
 logo:{position:"top-right",margin:"25",file:"https://github.com/HeNy007/AliwixPlayer/raw/main/2.png",height:"25"}, 
 flashplayer:"https://raw.githack.com/HeNy007/HeNy-Ben-Hamed/master/today-matches/jwplayer.flash.swf",tracks:{file:"thumbnails.vtt",kind:"thumbnails"} });
 
 
-jwplayer().addButton(
-    "/assets/download.svg",
-    "Download Video", 
-    function() {
-      window.location.href = jwplayer().getPlaylist()[0].sources[0].file;
+playerInstance.addButton(
+    'https://raw.githubusercontent.com/HeNy007/AliwixPlayer/main/assets/download.svg',
+    'Download video', 
+    function() {  
+    window.open(playerInstance.getPlaylistItem()['file']+'?type=video/mp4&title=Download%20video', '_blank').blur();
+    //window.location.href = playerInstance.getPlaylistItem()['file'];
     },
-    "download"
-);
+    'download'
+    );
+   
+   $(window).on("orientationchange", () => {
+   if(screen.width > screen.height){
+   // change to landscape
+   } else {
+   // change to portrait
+   }
+   });
