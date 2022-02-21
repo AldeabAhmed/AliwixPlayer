c700a61fb0275a2f2cf60f7c17093aaf48c9931d
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2022-02-21 22:11:14 +0100

    Update heny007-ad.js

--- AliwixPlay/heny007-ad.js
+++ AliwixPlay/heny007-ad.js
@@ -5,7 +5,7 @@ document.writeln("</style>");
 document.writeln("<div id=\"ad_asd\" class=\"slide_in\" style=\"display: none;\">");
 document.writeln("<span title=\"Close\" id=\"close_ad\">X</span>");
 document.writeln("<div class=\"center_ad\">");
-document.writeln("<iframe src=\"https://ad2bitcoin.com/ad.php?ref=heny007&width=300\" marginwidth=\"0\" marginheight=\"0\" width=\"300\" height=\"250\" scrolling=\"no\" border=\"0\" frameborder=\"0\"></iframe>");
+document.writeln("<iframe src=\"https://aliwix-lite.blogspot.com/p/ads.html&width=300\" marginwidth=\"0\" marginheight=\"0\" width=\"300\" height=\"250\" scrolling=\"no\" border=\"0\" frameborder=\"0\"></iframe>");
 document.writeln("</div>");
 document.writeln("</div>");
 var elem=document.getElementById("close_ad");elem.addEventListener("click touchstart",handler,!1);elem.addEventListener("click",handler,!1);function handler(b){var a=document.getElementById("ad_asd");b=a.classList.contains("slide_in");setTimeout(function(){a.parentNode.removeChild(a)},500);a.setAttribute("class",b?"slide_out":"slide_in")};setTimeout(function(){document.getElementById("ad_asd").style.display="block"},3E3);
