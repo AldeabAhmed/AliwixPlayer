a91ae189c59745ceb234e1b3960637e70ed0afdd
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2022-11-28 15:24:11 +0100

    Update heny007-ad.js

--- AliwixPlay/heny007-ad.js
+++ AliwixPlay/heny007-ad.js
@@ -5,8 +5,8 @@ document.writeln("</style>");
 document.writeln("<div id=\"ad_asd\" class=\"slide_in\" style=\"display: none;\">");
 document.writeln("<span title=\"Close\" id=\"close_ad\">X</span>");
 document.writeln("<div class=\"center_ad\">");
-document.writeln("ads here")
-document.writeln("</div>")
+document.writeln("ads here");
+document.writeln("</div>");
 document.writeln("</div>");
 var elem=document.getElementById("close_ad");elem.addEventListener("click touchstart",handler,!1);elem.addEventListener("click",handler,!1);function handler(b){var a=document.getElementById("ad_asd");b=a.classList.contains("slide_in");setTimeout(function(){a.parentNode.removeChild(a)},500);a.setAttribute("class",b?"slide_out":"slide_in")};setTimeout(function(){document.getElementById("ad_asd").style.display="block"},3E3);
   
